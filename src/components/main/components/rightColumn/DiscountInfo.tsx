78cf02a9504208fb23c18f61b80a2f1dbbe3e1df
pedrorosadomingues <pedrorosadomingues@gmail.com>
2023-09-24 19:25:04 -0300

    feat: discountInfo initiate

--- src/components/main/components/rightColumn/DiscountInfo.tsx
+++ src/components/main/components/rightColumn/DiscountInfo.tsx
@@ -1,24 +1,22 @@
 export default function DiscountInfo() {
     return (
         <div>
+            <div className="mt-[32px] flex bg-thirdBlue rounded-[10px] pt-[12px] pb-[12px] pl-[16px] pr-[16px] items-center">
+                <img src='images/main/ninthImage.svg'></img><span className=" ml-[16px] text-black font-primary size-[16px] font-light leading[22ox] ">Save <span className="text-secondaryBlue"> 53%</span> and get 6 extra Clarifision for only  <span className="text-secondaryBlue"> $14 Each</span>.</span>
+            </div>
+            <button></button>
             <div>
-                <div>
-                    <img></img><span></span>
-                </div>
-                <button></button>
-                <div>
-                    <span></span>
-                    <img></img>
-                    <img></img>
-                    <span></span>
-                    <img></img>
-                    <img></img>
-                </div>
-                <a></a>
-                <div>
-                    <img></img>
-                    <p></p>
-                </div>
+                <span></span>
+                <img></img>
+                <img></img>
+                <span></span>
+                <img></img>
+                <img></img>
+            </div>
+            <a></a>
+            <div>
+                <img></img>
+                <p></p>
             </div>
         </div>
     )
--- src/components/main/components/rightColumn/RightColumn.tsx
+++ src/components/main/components/rightColumn/RightColumn.tsx
@@ -1,9 +1,11 @@
+import DiscountInfo from "./DiscountInfo";
 import ProductInfo from "./ProductInfo";
 
 export default function RightColumn(): JSX.Element {
     return (
         <div className="w-[44%] ml-[40px] mb-[40px] mt-[30px]">
             <ProductInfo />
+            <DiscountInfo />
         </div>
     )
 
