e836533e85e1d793f1650f3c6e10f02a333adff7
pedrorosadomingues <pedrorosadomingues@gmail.com>
2023-09-25 11:57:20 -0300

    feat: test finished

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer(): JSX.Element {
                     <img className="ml-[12px] md:ml-[16px]  md:mr-[16px] mr-[12px] h-[14px] md:h-[16px]" src='/images/main/twoelfthImage.svg'/>
                     <span className="lowercase text-[12px] md:text-[16px] font-light font-primary md:leading-[22px] leading-[16px]">Clarifionsupport@clarifion.com</span>
                 </div>
-                <div className="flex items-center mt-[12px] md:mt-[24px] md:mt-[0px]">
+                <div className="flex items-center mt-[12px]  md:mt-[0px]">
                     <img className="mr-[12px] md:mr-[16px] md:w-[16px] w-[13px]" src='/images/main/footerPadlock.svg'/>
                     <span className="capitalize text-[12px] md:text-[16px] font-light font-primary md:leading-[22px] leading-[16px]">Secure 256-bit SSL encryption.</span>
                 </div>
